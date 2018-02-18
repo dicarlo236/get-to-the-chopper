2efa68ed8a9e321758c23cbfd8e8c53a12263c4b
Jared <dicarloj@mit.edu>
2018-02-18 02:57:43 -0500

    derpomatic

--- test.c
+++ test.c
@@ -1,7 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-
 // LOW SIDE CHOPPER FIRMWARE
 // requires atmega328pb "pack"
 // avrdude modification from "attila zoltan kovacs"
@@ -16,12 +15,9 @@
 
 // one "tick" is 31.25 us long
 
-// exceeding in_min/max results in a zero output
 #define IN_MIN 32        // minimum expected pwm in pulse length (ticks)
-#define IN_MAX 80        // maximum expected pwm in pulse length (ticks)
+#define IN_MAX 70        // maximum expected pwm in pulse length (ticks)
 
-// between the maximums and the deadbacks, the output is either full on or full off
-// avoid switching when we don't need to
 #define DEADBAND_MIN 36  // values less than this result in 0 output
 #define DEADBAND_MAX 60  // values larger than this (but less than in_max) result in full on
 #define OUT_MIN 0        // minimum duty cycle (out of 255)
@@ -30,90 +26,34 @@
 #define MIN_EDGES 2      // minimum number of edges required to make watchdog happy
 #define MAX_EDGES 12      // maximum number of edges required to make watchdog happy
 
-#define NOTE_1 (92*2*4)
-#define NOTE_2 (82*2*4)
-#define NOTE_3 (73*2*4)
-#define NOTE_4 (61*2*4)
+#define MAX_DOGS 3
 
-// number of PWM_IN timer overflows
-uint8_t dog_counter = 0;
-// has valid PWM in recently
 uint8_t has_pwm = 0;
-// number of PWM in edges
-uint8_t edges = 0;
 
-uint8_t is_startup = 1;
-
-
-uint8_t filtered_value = 0;
-
-// map PWM pulse duration to output
-uint8_t map(uint8_t x, uint8_t in_min, uint8_t in_max, uint8_t out_min, uint8_t out_max)
+uint8_t get_desired_pwm(uint8_t timer_ticks)
 {
-    if(x < DEADBAND_MIN) return 0;
-    if(x > IN_MAX) return 0;
-    if(x > DEADBAND_MAX) return OUT_MAX;
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    if(timer_ticks < DEADBAND_MIN) return 0;
+    if(timer_ticks > IN_MAX) return 0;
+    if(timer_ticks > DEADBAND_MAX) return OUT_MAX;
+    return (timer_ticks - DEADBAND_MIN) * (OUT_MAX - OUT_MIN) / (DEADBAND_MAX - DEADBAND_MIN) + OUT_MIN;
 }
 
-// remove output pin from timer 1
-void safe_output_pin()
+void disable()
 {
     TCCR1A = 0;
     TCCR1B = 0;
     PORTB = 0;
 }
 
-// set output pin to work with timer 1
-void danger_output_pin()
+void enable()
 {
     TCCR1A |= (1 << COM1A1)|(1 << COM1B1);
     TCCR1A |= (1 << WGM11);
     TCCR1B |= (1 << WGM12)|(1 << WGM13);
+    TCCR1B |= (1 << CS10);
 }
 
-void delay_ms(uint8_t ms) {
-    uint16_t delay_count = F_CPU / 17500;
-    volatile uint16_t i;
-
-    while (ms != 0) {
-        for (i=0; i != delay_count; i++);
-        ms--;
-    }
-}
-
-
-void note_delay()
-{
-    delay_ms(12);
-}
-
-void hobby_king()
-{
-    delay_ms(24);
-    OCR1A = 0x10;
-    danger_output_pin();
-    ICR1 = NOTE_1;
-    note_delay();
-    ICR1 = NOTE_2;
-    note_delay();
-    ICR1 = NOTE_3;
-    note_delay();
-    ICR1 = NOTE_4;
-    note_delay();
-    note_delay();
-    ICR1 = NOTE_3;
-    note_delay();
-    ICR1 = NOTE_4;
-    note_delay();
-    note_delay();
-    ICR1 = 0xff;
-    safe_output_pin();
-}
-
-
-// initialize all timer stuff
-void init_pwm()
+void init_timers()
 {
     // set gate out as output pin
     DDRB |= (1 << DDB1)|(1 << DDB2);
@@ -148,111 +88,54 @@ void init_pwm()
     sei();
 }
 
-// value of the pwm in pin
-uint8_t k = 0;
-// last value of the pwm in pin
-uint8_t last_k = 0;
-// value of timer when pwm in goes high
-uint8_t timer_start = 0; 
-// current time - start time
-uint8_t t_diff = 0;
 
-// timer 2 overflow (watchdog)
+uint8_t num_overflows = 0;
+uint8_t num_edges = 0;;
 ISR(TIMER2_OVF_vect)
 {
-    if(is_startup)
+    num_overflows++;
+    if(num_overflows > MAX_DOGS)
     {
-        danger_output_pin();
-        return;
-    }
-    // increment number of overflows
-    dog_counter++;
-    // if we've had enough, check edges
-    if(dog_counter >= MAX_DOGS)
-    {
-        dog_counter = 0;
-        if(edges > MAX_EDGES || edges < MIN_EDGES)
-        {
-            filtered_value = 0;
-            has_pwm = 0;
-        }
-        else
-            has_pwm = 1;
-
-        edges = 0;
+        num_overflows = 0;
+        has_pwm = (num_edges > 2);
+        num_edges = 0;
     }
 
-    // if we failed, go to failsafe mode
-    if(!has_pwm)
-        safe_output_pin();
-    else
-        danger_output_pin();
 }
 
-// pin change interrupt
+
+uint8_t timer_start = 0;
 ISR(PCINT2_vect)
 {
-    if(is_startup)
-    {
-        TCCR1B |= (1 << CS10);
-        return;
-    }
-    // value of pwm in pin
-    k = PIND & (1 <<PD2);
+    num_edges++;
 
+    if(!has_pwm) return;
 
-    // if it has toggled, increment edges
-    if(k ^ last_k)
-        edges++;
+    uint8_t pin_value = PIND & (1 << PD2);
 
-    // timer values
-    if(k)
+    if(pin_value)
         timer_start = TCNT2;
     else
     {
-        if(timer_start > TCNT2)
-        {
-            t_diff = ((int16_t)(TCNT2) + 0xff) - timer_start;
-        }
+        uint8_t t_diff = TCNT2 - timer_start;
+        uint8_t pwm_des = get_desired_pwm(t_diff);
+        if(pwm_des == 0)
+            disable();
         else
-            t_diff = TCNT2 - timer_start;
+        {
+            enable();
+            OCR1A = pwm_des;
+        }
     }
 
-    //compute pwm duty cycle desired
-    uint8_t pwm_des = map(t_diff,DEADBAND_MIN,DEADBAND_MAX,OUT_MIN,OUT_MAX);
-
-
-    filtered_value = pwm_des;
-
-
-    // if it's zero, turn off PWM
-    if(!pwm_des)
-    {
-        TCCR1B &= ~(1 << CS10);
-        PORTB &= ~(1 << PB1);
-        OCR1A = 0;
-    }
-    else if(has_pwm)
-    {
-        OCR1A = pwm_des;
-        TCCR1B |= (1 << CS10);
-    }
-    else
-        OCR1A = 0;
-
-    last_k = k;
 }
 
-int main(void) {
-    // initialize the direction of PORTD #6 to be an output
-    //set_output(DDRB, LED);  
-    DDRB |= (1 << DDB1)|(1 << DDB2);
-    safe_output_pin();
-    init_pwm();
-    hobby_king();
-    is_startup = 0;
-
+void main(void)
+{
+    init_timers();
     for(;;)
     {
+        if(!has_pwm)
+            disable();
     }
 }
